cd7f5d03e3e31313c5b7634d2438fec66254ea37
SavinovPetr <60872916+bask3tball1st@users.noreply.github.com>
2022-08-09 15:20:41 +0000

    Исправил ошибки

--- .ccls-cache/@home@runner@CheckPNG/main.cpp
+++ .ccls-cache/@home@runner@CheckPNG/main.cpp
@@ -1,35 +1,35 @@
-#include <iostream>
-#include <fstream>
-#include <string>
 #include <cstring>
+#include <fstream>
+#include <iostream>
+#include <string>
 
 int main() {
-	std::ifstream in;
-	std::string fileName;
-	std::cout << "Enter file path: ";
-	std::cin >> fileName;
-	int pos = fileName.find(".");
-	if (fileName.substr(pos + 1) == "png") {
-		in.open("" + fileName + "", std::ios::binary);
-		if (in.is_open()) {
+  std::ifstream in;
+  std::string fileName;
+  std::cout << "Enter file path: ";
+  std::cin >> fileName;
+  int pos = fileName.find(".");
+  if (fileName.substr(pos + 1) == "png") {
+    in.open("" + fileName + "", std::ios::binary);
+    if (in.is_open()) {
       char png[] = {-119, 'P', 'N', 'G', 0};
-			while (in.good()) {
-				char buffer[5];
-				in.read(buffer, sizeof(buffer));
-				buffer[4] = 0;
-				if (std::strcmp(buffer, png) == 0) {
-					std::cout << "The file was successfully checked for png!" << std::endl;
-					break;
-				}
-				else {
-					std::cout << "The file did not pass the PNG check!" << std::endl;
-					break;
-				}
-			}
-		}
-	}
-	else
-		std::cout << "The file is not PNG extension!" << std::endl;
-	in.close();
-	return 0;
+      while (in.good()) {
+        char buffer[5];
+        in.read(buffer, sizeof(buffer));
+        buffer[4] = 0;
+        if (std::strcmp(buffer, png) == 0) {
+          std::cout << "The file was successfully checked for png!"
+                    << std::endl;
+          break;
+        } else {
+          std::cout << "The file did not pass the PNG check!" << std::endl;
+          break;
+        }
+      }
+    } else
+      std::cout << "Invalid file path!" << std::endl;
+  } else
+    std::cout << "The file is not PNG extension!" << std::endl;
+  in.close();
+  return 0;
 }
